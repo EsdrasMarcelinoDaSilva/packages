e12b59cb9fdccece3d11ab3aa98e76f711e84e62
EsdrasMarcelinoDaSilva <94310938+EsdrasMarcelinoDaSilva@users.noreply.github.com>
2024-09-21 18:47:15 -0300

    feat: modified routes

--- src/com/patterns/strategy/NavigatorStrategy.java
+++ src/com/patterns/strategy/NavigatorStrategy.java
@@ -1,4 +1,22 @@
 package com.patterns.strategy;
 
 public class NavigatorStrategy {
+    public static void main(String[] args) {
+        Navigator navigator = new Navigator();
+
+        navigator.setRouteStrategy(new CarRouteStrategy());
+        navigator.buildRoute("A", "B");
+
+        navigator.setRouteStrategy(new BicycleRouteStrategy());
+        navigator.buildRoute("C", "D");
+
+        navigator.setRouteStrategy(new WalkingRouteStrategy());
+        navigator.buildRoute("E", "F");
+
+        navigator.setRouteStrategy(new PublicTransportRouteStrategy());
+        navigator.buildRoute("I", "J");
+
+        navigator.setRouteStrategy(new TouristAttractionRouteStrategy());
+        navigator.buildRoute("L", "M");
+    }
 }
